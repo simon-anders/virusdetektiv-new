c43b6af84e044657ee6968e6534dbbf860fd9e85
Simon Anders <sanders@fs.tum.de>
2021-05-05 23:52:52 +0200

    various

--- virdet_website/virdet_website/models.py
+++ virdet_website/virdet_website/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.contrib import admin
 
+class TokenList(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    comment = models.CharField(max_length=300)
+
 class Token(models.Model):
     code = models.CharField(max_length=20)
     used = models.BooleanField(default=False)
 
+class Order(models.Model):
+    token = models.ForeignKey(Token,on_delete=models.SET_NULL,null=True)
+    nKits = models.IntegerField()
+    encrypted_data = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+admin.site.register(TokenList)
 admin.site.register(Token)
+admin.site.register(Order)
--- virdet_website/virdet_website/views.py
+++ virdet_website/virdet_website/views.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import *
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'astrals/index.html')
 
 def token(request):
     if request.method != "POST":
